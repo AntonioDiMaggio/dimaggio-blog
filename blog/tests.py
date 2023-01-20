a5db8ae68bbcf075de9d76471e5d5d5cfa15c4b3
Antonio DiMaggio <antoniomdimaggio@gmail.com>
2023-01-19 20:49:33 -0800

    Continuous Integration

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(title="django", author="django", slug="django")
+
+    def test_post_model(self):
+        data = self.blog
+        self.assertTrue(isinstance(data, Post))
+        self.assertEqual(str(data), "django")
